606f6aedbf43c78113f21dbe77e4f704105841a3
aghamad <201467122@collegeahuntsic.qc.ca>
2016-11-13 20:27:01 -0500

    Added Detail action method to ComicBooksController

---
+++ ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBookGallery.Controllers
+{
+    public class ComicBooksController: Controller
+    {
+        // Controller classes need to be public
+
+        // Action Method
+        public string Detail() {
+            return "Hello from the ComicBookController";
+        }
+    }
+}
